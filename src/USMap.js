0707f1a2a09e2dd728b91ba23259ed23d93b19b7
Theo <alair999@live.com>
2023-11-25 12:30:09 -0500

    v

--- src/USMap.js
+++ src/USMap.js
@@ -1,16 +1,40 @@
 import React from "react"
-import { MapContainer, TileLayer } from "react-leaflet"
+import { MapContainer, TileLayer, GeoJSON } from "react-leaflet"
 import "leaflet/dist/leaflet.css"
 import "./USMap.css"
+import usParks from "./us-parks.json"
+import L from "leaflet"
 
 const USMap = () => {
+  // Customize the marker icon
+  const pointToLayer = (feature, latlng) => {
+    return L.circleMarker(latlng, {
+      radius: 1,
+      color: "#007FFF",
+      fillColor: "#007FFF",
+      fillOpacity: 1,
+    })
+  }
+
+  // Function to bind a popup to each feature
+  const onEachFeature = (feature, layer) => {
+    if (feature.properties && feature.properties.NAME) {
+      layer.bindPopup(feature.properties.NAME)
+    }
+  }
+
   return (
     <MapContainer
-      center={[39.0997, -94.5786]}
+      center={[37.8, -96]}
       zoom={4}
-      style={{ height: "500px", width: "70%", margin: "20 auto" }}
+      style={{ height: "500px", width: "70%", margin: "0 20px" }}
     >
       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      <GeoJSON
+        data={usParks}
+        pointToLayer={pointToLayer}
+        onEachFeature={onEachFeature}
+      />
     </MapContainer>
   )
 }
